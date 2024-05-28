96aa6f21f8cb1c485ab213e76ce76593381c103c
Julien2195 <j.lemaitre24@gmail.com>
2024-05-28 15:21:17 +0200

    exo 2

--- exo1.dart
+++ exo1.dart
@@ -7,25 +7,24 @@ void main() {
 }
 
 class ExoOne {
-
   void write() {
     try {
-        print("Enter your name?");
-        print("Entre votre age");
+      print("Enter your name?");
+      print("Entre votre age");
     } catch (e) {
       print('Erreur: $e');
     }
   }
 
   void read() {
-      try {
-        String name = stdin.readLineSync();
-        String ageInput = stdin.readLineSync();
+    try {
+      String name = stdin.readLineSync();
+      String ageInput = stdin.readLineSync();
       int age = int.tryParse(ageInput);
       int ageLeft = 100 - age;
-    print("Hello $name!, $ageLeft!");
+      print("Hello $name!, $ageLeft!");
     } catch (e) {
-        print('Erreur: $e');
+      print('Erreur: $e');
     }
   }
 }
