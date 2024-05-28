f28b681ef846fc4252ca5d22a8a7f27f83afe2eb
Julien2195 <j.lemaitre24@gmail.com>
2024-05-28 15:01:29 +0200

    exo 1

---
+++ exo1.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  ExoOne field = ExoOne();
+  field.write();
+  field.read();
+}
+
+class ExoOne {
+
+  void write() {
+    try {
+        print("Enter your name?");
+        print("Entre votre age");
+    } catch (e) {
+      print('Erreur: $e');
+    }
+  }
+
+  void read() {
+      try {
+        String name = stdin.readLineSync();
+        String ageInput = stdin.readLineSync();
+      int age = int.tryParse(ageInput);
+      int ageLeft = 100 - age;
+    print("Hello $name!, $ageLeft!");
+    } catch (e) {
+        print('Erreur: $e');
+    }
+  }
+}
