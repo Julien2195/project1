5ac2e07072ba700ab2d9ab79547d846188785343
Julien2195 <j.lemaitre24@gmail.com>
2024-05-30 14:46:26 +0200

    .

---
+++ my_app/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "premier",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    margin: EdgeInsets.all(16.0),
+                  ),
+                ),
+              ]),
+              Row(children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Deuxieme",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    margin: EdgeInsets.all(16.0),
+                  ),
+                ),
+              ]),
+              Row(children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "troisieme",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    margin: EdgeInsets.all(16.0),
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
