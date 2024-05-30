7d26f0eb81484cd9373525c4101e27a291c3bfeb
Julien2195 <j.lemaitre24@gmail.com>
2024-05-30 10:57:05 +0200

    .

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -9,10 +9,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('hello'),),
-        body: Container(color: Colors.red,),
-      ),
-    
+          appBar: AppBar(
+            title: Text('hello'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.purple,
+                child: Text("Premier",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white)),
+              ),
+              
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ],
+          )),
     );
   }
 }
